69f17a447ae427cfc12a27ca215a4f1ef56f8ecb
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2017-11-12 11:49:12 -0800

    ask dialog errors

--- src/commands/askDialogHandler.js
+++ src/commands/askDialogHandler.js
@@ -16,29 +16,7 @@ const YELLOW = "ffcc00"
 var dbURL = process.env.ELEPHANTSQL_URL
 
 const handler = (payload, res) => {
-    const msgDefaults = {
-		  response_type: 'in_channel',
-		  username: 'MrBoneyPantsGuy',
-		  icon_emoji: config('ICON_EMOJI')
-		}
-    
-    let attachments = [
-			{
-				title: "Bones Here!",
-				color: RED,
-				text: "hi",
-			}
-		]
-    
-    let msg = _.defaults({
-		channel: payload.channel_name,
-		attachments: attachments
-	  }, msgDefaults)
-    
-	  res.set('content-type', 'application/json')
-	  res.status(200).json(msg)
-    return
-    /*res.send('');
+    res.send('');
     var correctIDStructure = /^<@.*>$/
     var title = payload.submission.title;
     var desc = payload.submission.description;
@@ -100,7 +78,7 @@ const handler = (payload, res) => {
             }
         } else {
             title = "*** ERROR ***"
-            text = ""
+            text = "Invalid User ID"
             buttons = ""
             color = RED
         }
@@ -132,6 +110,6 @@ const handler = (payload, res) => {
                     console.error(err);
                 });
             });
-    });*/
+    });
 }
 module.exports = { pattern: /askDialogHandler/ig, handler: handler }
