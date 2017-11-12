b802e8aca7bcb5093a045dc29db34cb4acd3cf3b
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2017-11-12 13:37:16 -0800

    ask dialog errors

--- src/commands/askDialogHandler.js
+++ src/commands/askDialogHandler.js
@@ -10,14 +10,13 @@ const qs = require('querystring')
 const axios = require('axios')
 const dateValidator = require('date-and-time')
 const RED = "ff0000"
-const GREEN = "33cc33"
 const YELLOW = "ffcc00"
 
 var dbURL = process.env.ELEPHANTSQL_URL
 
 const handler = (payload, res) => {
     res.send('');
-    var correctIDStructure = /^<@.*>$/
+    var correctIDStructure = /^<@.*>$/;
     var title = payload.submission.title;
     var desc = payload.submission.description;
     var receiver = payload.submission.receiver;
@@ -25,7 +24,79 @@ const handler = (payload, res) => {
     var text = "";
     var color = "";
     var sid = "";
-    var buttons = [
+    var buttons = "";
+    
+    pg.connect(dbURL, function(err, client, done) {
+        if(err) {
+            console.log(err);
+        }
+        if(correctIDStructure.test(receiver)){
+        
+            if(payload.submission.due){
+                let currentDate = new Date();
+                let dueDate = new Date(payload.submission.due);
+                if(dateValidator.isValid(payload.submission.due, 'MMM D YYYY H:mm') && (currentDate - dueDate) < 0) {
+                    text = "Hey " + receiver + "! " + sender + " asked you to: \n" + desc + " by " + payload.submission.due
+                    color = YELLOW  
+                    client.query("INSERT INTO ASK_TABLE (RECEIVER_ID, SENDER_ID, REQ_DESC, TITLE, DUE_DATE) VALUES ($1, $2, $3, $4, $5) RETURNING serial_id", [receiver, sender, desc, title, payload.submission.due], function(err, result) {
+                        done();
+                        if(err) {
+                            console.log(err);
+                        }
+                        sid =  result.rows[0].serial_id;
+                    })
+                } else {
+                    title = "*** ERROR ***"
+                    text = "Invalid Date!"
+                    buttons = ""
+                    color = RED
+                }
+            } else {
+                text = "Hey " + receiver + "! " + sender + " asked you to: \n" + desc
+                color = YELLOW
+                
+                client.query("INSERT INTO ASK_TABLE (RECEIVER_ID, SENDER_ID, REQ_DESC, TITLE) VALUES ($1, $2, $3, $4) RETURNING serial_id", [receiver, sender, desc, title], function(err, result) {
+                    done();
+                    if(err) {
+                        console.log(err);
+                    }
+                    sid =  result.rows[0].serial_id;
+                })
+            }
+        } else {
+            title = "*** ERROR ***"
+            text = "Invalid User ID"
+            buttons = ""
+            color = RED
+        }
+        
+        console.log(sid);
+        if(color === YELLOW) {
+            setButtons();
+        }
+        
+        axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+            token: config('OAUTH_TOKEN'),
+            channel: payload.channel.id,
+            //text: 'Request sent!',
+            attachments: JSON.stringify([{
+                title: title,
+                color: color,
+                text: text,
+                fallback: "Something went wrong :/",
+                callback_id: "askDialogHandler",
+                actions: buttons,
+            }]),
+        })).then((result) => {
+            console.log('sendConfirmation: ', result.data);
+        }).catch((err) => {
+            console.log('sendConfirmation error: ', err);
+            console.error(err);
+        });
+    });
+    
+    function setButtons(){
+        buttons = [
                     {
                         name: "accept",
                         text: "Accept",
@@ -53,63 +124,6 @@ const handler = (payload, res) => {
                         }
                     }
                   ];
-    
-    pg.connect(dbURL, function(err, client, done) {
-        if(err) {
-            console.log(err);
-        }
-        if(correctIDStructure.test(receiver)){
-        
-            if(payload.submission.due){
-                let currentDate = new Date();
-                let dueDate = new Date(payload.submission.due);
-                if(dateValidator.isValid(payload.submission.due, 'MMM D YYYY H:mm') && (currentDate - dueDate) < 0) {
-                    text = "Hey " + receiver + "! " + sender + " asked you to: \n" + desc + " by " + payload.submission.due
-                    color = YELLOW
-                } else {
-                    title = "*** ERROR ***"
-                    text = "Invalid Date!"
-                    buttons = ""
-                    color = RED
-                }
-            } else {
-                text = "Hey " + receiver + "! " + sender + " asked you to: \n" + desc
-                color = YELLOW
-            }
-        } else {
-            title = "*** ERROR ***"
-            text = "Invalid User ID"
-            buttons = ""
-            color = RED
-        }
-
-            client.query("INSERT INTO ASK_TABLE (RECEIVER_ID, SENDER_ID, REQ_DESC, TITLE, DUE_DATE) VALUES ($1, $2, $3, $4, $5) RETURNING serial_id", [receiver, sender, desc, title, payload.submission.due], function(err, result) {
-                done();
-
-                if(err) {
-                    console.log(err);
-                }
-
-                sid =  result.rows[0].serial_id;
-                axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
-                    token: config('OAUTH_TOKEN'),
-                    channel: payload.channel.id,
-                    //text: 'Request sent!',
-                    attachments: JSON.stringify([{
-                        title: title,
-                        color: color,
-                        text: text,
-                        fallback: "Something went wrong :/",
-                        callback_id: "askDialogHandler",
-                        actions: buttons,
-                    }]),
-                })).then((result) => {
-                    console.log('sendConfirmation: ', result.data);
-                }).catch((err) => {
-                    console.log('sendConfirmation error: ', err);
-                    console.error(err);
-                });
-            });
-    });
+    }
 }
 module.exports = { pattern: /askDialogHandler/ig, handler: handler }
