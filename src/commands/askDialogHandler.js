dd781f1c3a2d14d77a2d43bb287811a0e722ae7a
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2017-11-13 15:01:15 -0800

    ask dialog errors

--- src/commands/askDialogHandler.js
+++ src/commands/askDialogHandler.js
@@ -21,8 +21,6 @@ const handler = (payload, res) => {
     var desc = payload.submission.description;
     var receiver = payload.submission.receiver;
     var sender = "<@" + payload.user.id + ">";
-    var text = "";
-    var color = "";
     var sid = "";
     var buttons = "";
     
@@ -35,9 +33,8 @@ const handler = (payload, res) => {
             if(payload.submission.due){
                 let currentDate = new Date();
                 let dueDate = new Date(payload.submission.due);
+                //Check if valid date format and that date hasn't already past
                 if(dateValidator.isValid(payload.submission.due, 'MMM D YYYY H:mm') && (currentDate - dueDate) < 0) {
-                    text = "Hey " + receiver + "! " + sender + " asked you to: \n" + desc + " by " + payload.submission.due
-                    color = YELLOW
                     client.query("INSERT INTO ASK_TABLE (RECEIVER_ID, SENDER_ID, REQ_DESC, TITLE, DUE_DATE) VALUES ($1, $2, $3, $4, $5) RETURNING serial_id", [receiver, sender, desc, title, payload.submission.due], function(err, result) {
                         done();
                         if(err) {
@@ -45,18 +42,12 @@ const handler = (payload, res) => {
                         }
                         sid =  result.rows[0].serial_id;
                         setButtons(sid);
-                        sendMessage(false);
+                        sendMessage(false, title, "Hey " + receiver + "! " + sender + " asked you to: \n" + desc + " by " + payload.submission.due, YELLOW);
                     })
                 } else {
-                    title = "*** ERROR ***"
-                    text = "Invalid Date!"
-                    buttons = ""
-                    color = RED
-                    sendMessage(true);
+                    sendMessage(true, "*** ERROR ***", "Invalid Date!", RED);
                 }
             } else {
-                text = "Hey " + receiver + "! " + sender + " asked you to: \n" + desc
-                color = YELLOW
                 client.query("INSERT INTO ASK_TABLE (RECEIVER_ID, SENDER_ID, REQ_DESC, TITLE) VALUES ($1, $2, $3, $4) RETURNING serial_id", [receiver, sender, desc, title], function(err, result) {
                     done();
                     if(err) {
@@ -64,15 +55,11 @@ const handler = (payload, res) => {
                     }
                     sid =  result.rows[0].serial_id;
                     setButtons(sid);
-                    sendMessage(false);
+                    sendMessage(false, title, "Hey " + receiver + "! " + sender + " asked you to: \n" + desc, YELLOW);
                 })
             }
         } else {
-            title = "*** ERROR ***"
-            text = "Invalid User ID"
-            buttons = ""
-            color = RED
-            sendMessage(true);
+            sendMessage(true, "*** ERROR ***", "Invalid User ID", RED);
         }
     });
     
@@ -107,13 +94,12 @@ const handler = (payload, res) => {
                   ];
     }
     
-    function sendMessage(isError){
+    function sendMessage(isError, title, text, color){
         if(isError){
             axios.post('https://slack.com/api/chat.postEphemeral', qs.stringify({
                 token: config('OAUTH_TOKEN'),
                 user: payload.user.id,
                 channel: payload.channel.id,
-                //text: 'Request sent!',
                 attachments: JSON.stringify([{
                     title: title,
                     color: color,
@@ -131,7 +117,6 @@ const handler = (payload, res) => {
             axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
                 token: config('OAUTH_TOKEN'),
                 channel: payload.channel.id,
-                //text: 'Request sent!',
                 attachments: JSON.stringify([{
                     title: title,
                     color: color,
