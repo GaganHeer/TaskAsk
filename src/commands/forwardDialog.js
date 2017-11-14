0a84af92e86928452d39cd4799a09f1005f36fc9
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2017-11-13 19:09:50 -0800

    forward dialog

--- src/commands/forwardDialog.js
+++ src/commands/forwardDialog.js
@@ -18,36 +18,44 @@ const handler = (payload, res) => {
     
     axios.post('https://slack.com/api/users.list', qs.stringify({
                 token: config('OAUTH_TOKEN'),
-            })).then((result) => {
-        var dialog = {
-      token: config('OAUTH_TOKEN'),
-      trigger_id,
-      dialog: JSON.stringify({
-        title: 'Forward A Task',
-        callback_id: 'forwardDialog',
-        submit_label: 'Forward',
-        elements: [
-          {
-            label: 'Receiever',
-            type: 'text',
-            name: 'receiver',
-            hint: 'The person you want to forward this task to'
-          },
-          {
-            label: 'Task#',
-            type: 'text',
-            name: 'task',
-            value: payload.actions[0].value,
-            hint: 'ID# of the task you are forwarding',
-          },
-        ],
-      }),
-    };
-                console.log('sendConfirmation: ', result.data);
+    })).then((result) => {
+        const dialog = {
+            token: config('OAUTH_TOKEN'),
+            trigger_id,
+            dialog: JSON.stringify({
+                title: 'Forward A Task',
+                callback_id: 'forwardDialog',
+                submit_label: 'Forward',
+                elements: [
+                    {
+                    label: 'Receiever',
+                    type: 'text',
+                    name: 'receiver',
+                    hint: 'The person you want to forward this task to'
+                    },
+                    {
+                    label: 'Task#',
+                    type: 'text',
+                    name: 'task',
+                    value: payload.actions[0].value,
+                    hint: 'ID# of the task you are forwarding',
+                    },
+                ],
+            }),
+        };
+        
+        axios.post('https://slack.com/api/dialog.open', qs.stringify(dialog))
+            .then((result) => {
+                console.log('dialog.open: ', result.data);
+                res.send('');
             }).catch((err) => {
-                console.log('sendConfirmation error: ', err);
-                console.error(err);
+                console.log('dialog.open call failed: %o', err);
+                res.sendStatus(500);
             });
+                console.log('sendConfirmation: ', result.data);
+    }).catch((err) => {
+        console.log('sendConfirmation error: ', err);
+    });
     
     /*const dialog = {
       token: config('OAUTH_TOKEN'),
@@ -73,15 +81,5 @@ const handler = (payload, res) => {
         ],
       }),
     };*/
-
-    // open the dialog by calling dialogs.open method and sending the payload
-    axios.post('https://slack.com/api/dialog.open', qs.stringify(dialog))
-      .then((result) => {
-        console.log('dialog.open: ', result.data);
-        res.send('');
-      }).catch((err) => {
-        console.log('dialog.open call failed: %o', err);
-        res.sendStatus(500);
-      });
 }
 module.exports = { pattern: /forwardDialog/ig, handler: handler }
