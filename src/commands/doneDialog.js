874889534b6f4c72b2bd22792e53f8fa25cf1c9d
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2017-11-17 14:44:27 -0800

    Done dialog

--- src/commands/doneDialog.js
+++ src/commands/doneDialog.js
@@ -32,27 +32,28 @@ const handler = (payload, res) => {
             console.log("LENGTH" + result.rows.length);
             console.log(result.rows[0].serial_id);
             console.log(result.rows[1].serial_id);
+            
+            const dialog = {
+                token: config('OAUTH_TOKEN'),
+                trigger_id,
+                dialog: JSON.stringify({
+                    title: 'Mark a task as done',
+                    callback_id: 'doneDialog',
+                    submit_label: 'Done',
+                    elements: [
+                        {
+                            "label": "Tasks",
+                            "type": "select",
+                            "name": "task",
+                            "options": acceptedList,
+                        },
+                    ],
+                }),
+            };
+
         })
     });
     
-    const dialog = {
-        token: config('OAUTH_TOKEN'),
-        trigger_id,
-        dialog: JSON.stringify({
-            title: 'Mark a task as done',
-            callback_id: 'doneDialog',
-            submit_label: 'Done',
-            elements: [
-                {
-                    "label": "Tasks",
-                    "type": "select",
-                    "name": "task",
-                    "options": acceptedList,
-                },
-            ],
-        }),
-    };
-
     axios.post('https://slack.com/api/dialog.open', qs.stringify(dialog))
         .then((result) => {
             console.log('dialog.open: ', result.data);
