a25878d41e6b20cec7de5700d40e06bffe4432bd
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2017-11-21 18:27:16 -0800

    done dialog

--- src/commands/doneDialog.js
+++ src/commands/doneDialog.js
@@ -22,41 +22,45 @@ const handler = (payload, res) => {
         if(err) {
             sendMessage("*** ERROR ***", err, RED);
         }
-        client.query("SELECT * FROM ASK_TABLE WHERE RECEIVER_ID = $1 AND STATUS = $2 ORDER BY SERIAL_ID DESC LIMIT 100", [receiver, ACCEPTED_STATUS], function(err, result) {
-            done();
-            if(err) {
-                sendMessage("*** ERROR ***", err, RED);
-            }
-            for (var i = 0; i < result.rows.length; i++) {
-                acceptedList[i] = {label: "ID# " + result.rows[i].serial_id + ": " + result.rows[i].title, value: result.rows[i].jira_id};
-		    }
-            
-            const dialog = {
-                token: config('OAUTH_TOKEN'),
-                trigger_id,
-                dialog: JSON.stringify({
-                    title: 'Mark a task as done',
-                    callback_id: 'doneDialog',
-                    submit_label: 'Done',
-                    elements: [
-                        {
-                            "label": "Accepted Tasks",
-                            "type": "select",
-                            "name": "task",
-                            "options": acceptedList,
-                        },
-                    ],
-                }),
-            };
-            axios.post('https://slack.com/api/dialog.open', qs.stringify(dialog))
-            .then((result) => {
-                console.log('dialog.open: ', result.data);
-                res.send('');
-            }).catch((err) => {
-                sendMessage("*** ERROR ***", err, RED);
-                res.sendStatus(500);
-            });
-        })
+        if(result.rows.length == 0){
+            sendMessage("*** ERROR ***", "No accepted requests to display", RED);
+        } else {
+            client.query("SELECT * FROM ASK_TABLE WHERE RECEIVER_ID = $1 AND STATUS = $2 ORDER BY SERIAL_ID DESC LIMIT 100", [receiver, ACCEPTED_STATUS], function(err, result) {
+                done();
+                if(err) {
+                    sendMessage("*** ERROR ***", err, RED);
+                }
+                for (var i = 0; i < result.rows.length; i++) {
+                    acceptedList[i] = {label: "ID# " + result.rows[i].serial_id + ": " + result.rows[i].title, value: result.rows[i].jira_id};
+                }
+
+                const dialog = {
+                    token: config('OAUTH_TOKEN'),
+                    trigger_id,
+                    dialog: JSON.stringify({
+                        title: 'Mark a task as done',
+                        callback_id: 'doneDialog',
+                        submit_label: 'Done',
+                        elements: [
+                            {
+                                "label": "Accepted Tasks",
+                                "type": "select",
+                                "name": "task",
+                                "options": acceptedList,
+                            },
+                        ],
+                    }),
+                };
+                axios.post('https://slack.com/api/dialog.open', qs.stringify(dialog))
+                .then((result) => {
+                    console.log('dialog.open: ', result.data);
+                    res.send('');
+                }).catch((err) => {
+                    sendMessage("*** ERROR ***", err, RED);
+                    res.sendStatus(500);
+                });
+            })
+        }
     });
     
     function sendMessage(title, text, color){
