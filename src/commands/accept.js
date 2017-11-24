cf06ce2548fb792d540e60ea4be9f2c34324a4a2
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2017-11-24 09:49:41 -0800

    Accept dialog

--- src/commands/accept.js
+++ src/commands/accept.js
@@ -40,7 +40,10 @@ const handler = (payload, res) => {
     var acceptingUserID;
 	var taskNumber;
 	var isButton = false;
-	
+    let clarifications = '';
+    let dbQ1 = "SELECT * FROM clarify_table WHERE serial_id = $1;";
+	let dbQ2 = "SELECT * FROM ask_table WHERE serial_id = $1";
+
     if(payload.hasOwnProperty('original_message')) {
 		console.log("BUTTON PRESSED TIME TO TRIM");
 		
@@ -49,156 +52,176 @@ const handler = (payload, res) => {
 		channelName = payload.channel.name;
 		isButton = true;
 	} else if(!onlyNumbers.test(payload.text)){
-        var wrongParamMsg = "Please enter the correct format /accept [ID#] \n ex) /accept 24"
-        var wrongParamTitle = "*** ERROR ***"
+        var wrongParamMsg = "Please enter the correct format /accept [ID#] \n ex) /accept 24";
+        var wrongParamTitle = "*** ERROR ***";
         createSendMsg(wrongParamTitle, wrongParamMsg, RED, ONLY_USER);
 	} else {
 		taskNumber = parseInt(payload.text);
 		acceptingUserID = "<@" + payload.user_id + ">";
 		channelName = payload.channel_name;
 	}
-	
-	pg.connect(dbURL, function(err, client, done) {		
-		if(err) {
-			createSendMsg("*** ERROR ***", err, RED, ONLY_USER);
-		}
-		client.query("SELECT * FROM ASK_TABLE WHERE SERIAL_ID = $1", [taskNumber], function(err, selectResult){
-			done();
-			if(err){
-				createSendMsg("*** ERROR ***", err, RED, ONLY_USER);
-			}
-			var taskNumberRow = selectResult.rows;
-			if(taskNumberRow.length == 0){
-				var falseIDMsg = taskNumber + " is not a valid ID#";
-				var falseIDTitle = "*** ERROR ***";
-				createSendMsg(falseIDTitle, falseIDMsg, RED, ONLY_USER);
+	pool.connect().then(client => {
+		return client.query(dbQ1, [taskNumber])
+			.then(result => {
+				client.release();
+				if (result.rows.length > 0) {
+                    for (let i=0; i<result.rows.length; i++) {
+                        clarifications += '\n\nQuestion:\n'+ result.rows[i].clar_quest + '\nAnswer:\n'+ result.rows[i].clar_answer;
+                    }
+				}
+				pool.connect().then(client1 => {
+					return client1.query(dbQ2, [taskNumber])
+						.then(result1 => {
+                            var taskNumberRow = result1.rows;
+                            if(taskNumberRow.length == 0){
+                                var falseIDMsg = taskNumber + " is not a valid ID#";
+                                var falseIDTitle = "*** ERROR ***";
+                                createSendMsg(falseIDTitle, falseIDMsg, RED, ONLY_USER);
 
-			} else if (taskNumberRow[0].status !== PENDING_STATUS) {
-				var notPendingMsg = acceptingUserID + " that task can't be accepted! it is currently [" + taskNumberRow[0].status + "]";
-				var notPendingTitle = "*** ERROR ***";
-				createSendMsg(notPendingTitle, notPendingMsg, RED, ONLY_USER);
+                            } else if (taskNumberRow[0].status !== PENDING_STATUS) {
+                                var notPendingMsg = acceptingUserID + " that task can't be accepted! it is currently [" + taskNumberRow[0].status + "]";
+                                var notPendingTitle = "*** ERROR ***";
+                                createSendMsg(notPendingTitle, notPendingMsg, RED, ONLY_USER);
 
-			} else if(taskNumberRow[0].receiver_id != acceptingUserID){
-				var invalidAcceptMsg = acceptingUserID + " that task is assigned to " + taskNumberRow[0].receiver_id + " not you!";
-				var invalidAcceptTitle = "*** ERROR ***";
-				createSendMsg(invalidAcceptTitle, invalidAcceptMsg, RED, ONLY_USER);
+                            } else if(taskNumberRow[0].receiver_id != acceptingUserID){
+                                var invalidAcceptMsg = acceptingUserID + " that task is assigned to " + taskNumberRow[0].receiver_id + " not you!";
+                                var invalidAcceptTitle = "*** ERROR ***";
+                                createSendMsg(invalidAcceptTitle, invalidAcceptMsg, RED, ONLY_USER);
 
-			} else {
-				jiraSummary = selectResult.rows[0].sender_id +' '+selectResult.rows[0].req_date;
-				let receiverSlackID = selectResult.rows[0].receiver_id;
-				let senderSlackID = selectResult.rows[0].sender_id;
-				let baseDesc = selectResult.rows[0].req_desc;
-				pool.connect().then(client => {
-					return client.query('SELECT * FROM user_table WHERE slack_id = $1 OR slack_id = $2', [receiverSlackID, senderSlackID])
-						.then(res => {
-						client.release();
-						console.log("Log 1: "+ res.rows[0].f_name);
-						for (let i=0; i<res.rows.length; i++){
-							if (res.rows[i].slack_id == senderSlackID){
-								senderSlackName = res.rows[i].f_name +' '+ res.rows[i].l_name;
-							}
-							if (res.rows[i].slack_id == receiverSlackID){
-								receiverSlackName = res.rows[i].f_name +' '+ res.rows[i].l_name;
-								jiraAssingee = res.rows[i].jira_name;
-							}
-						}
-						jiraDesc = 'Assigned by: '+ senderSlackName +'\n\n'+'Assigned to: '+ receiverSlackName +'\n\n'+ baseDesc;
-						jiraIssue = {
-							"fields": {
-								"project": {
-									"id": "13100"  //TASK PROJECT ID
-								},
-								"summary": jiraSummary,
-								"issuetype": {
-									"id": "3"  //'Task' issue type
-								},
-								"assignee": {
-									"name": jiraAssingee
-								},
-								"description": jiraDesc,
-								"customfield_10100": [{  //Custom Field for Review Type.
-									"id": "10103"  //All slack task will not require review.
-								}]
-							}
-						};
-						console.log("Log 2 :"+ jiraIssue);
-						jira.addNewIssue(jiraIssue, function (error, issue){
-							if (error){
-								console.log(error);
-								return(error);
-							} else {
-								console.log('Console log 3, Jira Key: ' + issue.key);
-								jiraKey = issue.key;
-								console.log('Console log 4: '+ jiraKey);
-								jira.transitionIssue(jiraKey, issueTrans, function (err, issueUpdate) {  //Changes the Issue's Status to In Progress
-									if (err) {
-										console.log(err);
-										return(err);
-									} else {
-										console.log("Console log 5, Jira Status change was a: "+ JSON.stringify(issueUpdate));
-										client.query("UPDATE ASK_TABLE SET STATUS = $1, JIRA_ID = $2 WHERE SERIAL_ID = $3", [ACCEPTED_STATUS, jiraKey, taskNumber], function (err, updateResult) {
-											client.query("SELECT * FROM ASK_TABLE WHERE SERIAL_ID = $1", [taskNumber], function (err2, selectResult) {
-												done();
-												if(err) {
-													createSendMsg("*** ERROR ***", err, RED, ONLY_USER);
+                            } else {
+                                jiraSummary = taskNumberRow[0].title;
+                                let receiverSlackID = taskNumberRow[0].receiver_id;
+                                let senderSlackID = taskNumberRow[0].sender_id;
+                                let baseDesc = taskNumberRow[0].req_desc;
+                                let dueDate = taskNumberRow[0].due_date;
+
+                                pool.connect().then(client => {
+                                    return client.query('SELECT * FROM user_table WHERE slack_id = $1 OR slack_id = $2', [receiverSlackID, senderSlackID])
+                                        .then(res => {
+                                            client.release();
+                                            console.log("Log 1: "+ res.rows[0].f_name);
+                                            for (let i=0; i<res.rows.length; i++){
+                                                if (res.rows[i].slack_id == senderSlackID){
+                                                    senderSlackName = res.rows[i].f_name +' '+ res.rows[i].l_name;
+                                                }
+                                                if (res.rows[i].slack_id == receiverSlackID){
+                                                    receiverSlackName = res.rows[i].f_name +' '+ res.rows[i].l_name;
+                                                    jiraAssingee = res.rows[i].jira_name;
+                                                }
+                                            }
+                                            jiraDesc = 'Assigned by: '+ senderSlackName +'\n\n'+'Assigned to: '+ receiverSlackName +'\n\n'+ baseDesc + clarifications;
+                                            jiraIssue = {
+                                                "fields": {
+                                                    "project": {
+                                                        "id": "13100"  //TASK PROJECT ID
+                                                    },
+                                                    "summary": jiraSummary,
+                                                    "issuetype": {
+                                                        "id": "3"  //'Task' issue type
+                                                    },
+                                                    "assignee": {
+                                                        "name": jiraAssingee
+                                                    },
+                                                    "description": jiraDesc,
+                                                    "duedate": dueDate,
+                                                    "customfield_10100": [{  //Custom Field for Review Type.
+                                                        "id": "10103"  //All slack task will not require review.
+                                                    }]
+                                                }
+                                            };
+                                            console.log("Log 2 :"+ jiraIssue);
+                                            jira.addNewIssue(jiraIssue, function (error, issue){
+                                                if (error){
+                                                    console.log(error);
+                                                    return(error);
+                                                } else {
+                                                    console.log('Console log 3, Jira Key: ' + issue.key);
+                                                    jiraKey = issue.key;
+                                                    console.log('Console log 4: '+ jiraKey);
+                                                    jira.transitionIssue(jiraKey, issueTrans, function (err, issueUpdate) {  //Changes the Issue's Status to In Progress
+                                                        if (err) {
+                                                            console.log(err);
+                                                            return(err);
+                                                        } else {
+                                                            console.log("Console log 5, Jira Status change was a: "+ JSON.stringify(issueUpdate));
+                                                            pool.connect().then(client => {
+                                                                client.query("UPDATE ASK_TABLE SET STATUS = $1, JIRA_ID = $2 WHERE SERIAL_ID = $3", [ACCEPTED_STATUS, jiraKey, taskNumber])
+																	.then(result => {
+                                                                        client.query("SELECT * FROM ASK_TABLE WHERE SERIAL_ID = $1", [taskNumber])
+																			.then(result2 => {
+                                                                                client.release();
+                                                                                taskNumberRow = result2.rows;
+                                                                                var acceptMsg = taskNumberRow[0].sender_id + "! " + taskNumberRow[0].receiver_id + " has accepted Task ID: " + taskNumberRow[0].serial_id + " '" + taskNumberRow[0].req_desc + "'";
+                                                                                var acceptTitle = "Task Accepted";
+                                                                                createSendMsg(acceptTitle, acceptMsg, GREEN, IN_CHANNEL);
+																			})
+																			.catch(err1 => {
+                                                                                console.log(err1);
+                                                                                createSendMsg("*** ERROR ***", err1, RED, ONLY_USER);
+																			});
+                                                                        })
+																		.catch(err => {
+																			console.log(err);
+																			createSendMsg("*** ERROR ***", err, RED, ONLY_USER);
+																		});
+															})
+														}
+													});
+
 												}
-												if(err2) {
-													createSendMsg("*** ERROR ***", err2, RED, ONLY_USER);
-												}
-												taskNumberRow = selectResult.rows;
-												var acceptMsg = taskNumberRow[0].sender_id + "! " + taskNumberRow[0].receiver_id + " has accepted " + taskNumberRow[0].jira_id + " '" + taskNumberRow[0].req_desc + "'";
-												var acceptTitle = "Task Accepted";
-												createSendMsg(acceptTitle, acceptMsg, GREEN, IN_CHANNEL);
 											});
-										});
-									}
-								});
-							}
-						});
-					})
-						.catch(e => {
-						client.release();
-						console.log(err.stack);
-					})
+                                        }).catch(e => {
+                                            client.release();
+                                            console.log(e.stack);
+                                        });
+                                });
+
+                                var finalUser;
+                                var finalUserId;
+                                var targetDM = senderSlackID.slice(2,11);
+
+
+                                axios.post('https://slack.com/api/im.list', qs.stringify({
+                                    token: config('POST_BOT_TOKEN'),
+
+                                })).then(function (resp){
+                                    console.log(resp.data);
+                                    for(var t = 0; t < resp.data.ims.length; t++){
+                                        console.log(t);
+                                        console.log(resp.data.ims[t].id);
+                                        if(targetDM==resp.data.ims[t].user){
+                                            finalUser = resp.data.ims[t].id;
+                                            finalUserId = resp.data.ims[t].user;
+                                            axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+                                                token: config('POST_BOT_TOKEN'),
+                                                channel: finalUser,
+                                                user:finalUserId,
+                                                as_user:true,
+                                                text: "Accepted by: "+receiverSlackID,
+
+                                            })).then((result) => {
+                                                console.log('sendConfirmation: ', result.data);
+                                            }).catch((err) => {
+                                                console.log('sendConfirmation error: ', err);
+                                                console.error(err);
+                                            });
+                                        }
+                                    }
+                                }).catch(function (err){
+                                    console.log(err);
+                                });
+                            }
+						})
+						.catch(err1 => {
+                            console.log(err1);
+                            createSendMsg("*** ERROR ***", err1, RED, ONLY_USER);
+                        });
 				});
-
-				var finalUser;
-				var finalUserId;
-				var targetDM = senderSlackID.slice(2,11);
-
-				
-
-				axios.post('https://slack.com/api/im.list', qs.stringify({
-				    token: config('POST_BOT_TOKEN'),
-				    
-				})).then(function (resp){
-					console.log(resp.data);
-					for(var t = 0; t < resp.data.ims.length; t++){
-						console.log(t);
-						console.log(resp.data.ims[t].id);
-						if(targetDM==resp.data.ims[t].user){
-							finalUser = resp.data.ims[t].id;
-							finalUserId = resp.data.ims[t].user;
-							axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
-							    token: config('POST_BOT_TOKEN'),
-							    channel: finalUser,
-							    user:finalUserId,
-							    as_user:true,
-							    text: "Accepted by: "+receiverSlackID,
-							   
-							})).then((result) => {
-							        console.log('sendConfirmation: ', result.data);
-							    }).catch((err) => {
-							        console.log('sendConfirmation error: ', err);
-							        console.error(err);
-							    });
-						}
-					}
-				}).catch(function (err){
-					console.log(err);
-				});
-			}
-		});
+			})
+			.catch(err => {
+				console.log(err);
+                createSendMsg("*** ERROR ***", err, RED, ONLY_USER);
+			});
 	});
 
     function createSendMsg(attachTitle, attachMsg, attachColor, respType){
