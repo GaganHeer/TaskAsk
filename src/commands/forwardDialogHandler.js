08757e7007299a4f28cb4a273164a681a54515fe
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2017-11-25 21:34:22 -0800

    forward dialog refactor

--- src/commands/forwardDialogHandler.js
+++ src/commands/forwardDialogHandler.js
@@ -24,115 +24,86 @@ const handler = (payload, res) => {
     var buttons = "";
     
     pool.connect().then(client => {
-        client.query("SELECT * FROM ASK_TABLE WHERE SERIAL_ID = $1", [taskNumber])
+        res.send('')
+        client.query("UPDATE ASK_TABLE SET RECEIVER_ID = $1 WHERE SERIAL_ID = $2 RETURNING *", [receiver, taskNumber])
             .then(result => {
                 client.release();
-            
-                var taskNumberRow = result.rows;
-                if(taskNumberRow.length == 0){
-                    res.send({
-                        "errors": [{
-                            "name": "task",
-                            "error": taskNumber + " is not a valid ID#"
-                        }]
-                    })
-                } else if(!(taskNumberRow[0].receiver_id === forwarder || taskNumberRow[0].sender_id === forwarder)){
-                    res.send({
-                        "errors": [{
-                            "name": "task",
-                            "error": "You haven't sent or received this request so you can't forward it"
-                        }]
-                    })
-                } else if (taskNumberRow[0].status !== PENDING_STATUS) {
-                    res.send({
-                        "errors": [{
-                            "name": "task",
-                            "error": "That task can't be forwarded! it is currently [" + taskNumberRow[0].status + "]"
-                        }]
-                    })
-                } else {
-                    res.send('')
-                    client.query("UPDATE ASK_TABLE SET RECEIVER_ID = $1 WHERE SERIAL_ID = $2 RETURNING *", [receiver, taskNumber])
-                        .then(result => {
-                            client.release();
-                        
-                            taskNumberRow = result.rows;
-                            setButtons(taskNumber);
-                            var finalUser;
-                            var finalUserId;
-                            var receivertargetDM = taskNumberRow[0].receiver_id.slice(2,11);
-                            var sendertargetDM = taskNumberRow[0].sender_id.slice(2,11);
 
-                            //DM to the new receiver of the task
-                            axios.post('https://slack.com/api/im.list', qs.stringify({
-                                token: config('POST_BOT_TOKEN'),
-                            })).then(function (resp){
-                                for(var t = 0; t < resp.data.ims.length; t++){
-                                    //console.log(resp.data.ims[t].id); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
-                                    if(receivertargetDM==resp.data.ims[t].user){
-                                        finalUser = resp.data.ims[t].id;
-                                        finalUserId = resp.data.ims[t].user;
-                                        axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
-                                            token: config('POST_BOT_TOKEN'),
-                                            channel: finalUser,
-                                            user:finalUserId,
-                                            as_user:true,
-                                            attachments: JSON.stringify([
-                                              {
-                                                title: "Forwarded",
-                                                color: ORANGE,
-                                                text: "Task ID: " + taskNumber + "\n Title: " + result.rows[0].title + "\n Recipient: " + result.rows[0].receiver_id +  " Forwarder: " + forwarder + " Owner: " + result.rows[0].sender_id,
-                                                callback_id: "askDialogHandler",
-                                                actions: buttons
-                                              },
-                                            ]),
-                                        })).then((result) => {
-                                            //console.log('sendConfirmation: ', result.data); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
-                                        }).catch((err) => {
-                                            //console.log('sendConfirmation error: ', err); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
-                                        });
-                                    }
-                                }
-                            }).catch(function (err){
-                                //console.log(err); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
-                            });
+                taskNumberRow = result.rows;
+                setButtons(taskNumber);
+                var finalUser;
+                var finalUserId;
+                var receivertargetDM = taskNumberRow[0].receiver_id.slice(2,11);
+                var sendertargetDM = taskNumberRow[0].sender_id.slice(2,11);
 
-                            //DM to the owner of the task
-                            axios.post('https://slack.com/api/im.list', qs.stringify({
+                //DM to the new receiver of the task
+                axios.post('https://slack.com/api/im.list', qs.stringify({
+                    token: config('POST_BOT_TOKEN'),
+                })).then(function (resp){
+                    for(var t = 0; t < resp.data.ims.length; t++){
+                        //console.log(resp.data.ims[t].id); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
+                        if(receivertargetDM==resp.data.ims[t].user){
+                            finalUser = resp.data.ims[t].id;
+                            finalUserId = resp.data.ims[t].user;
+                            axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
                                 token: config('POST_BOT_TOKEN'),
-                            })).then(function (resp){
-                                //console.log(resp.data); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
-                                for(var t = 0; t < resp.data.ims.length; t++){
-                                    //console.log(resp.data.ims[t].id); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
-                                    if(sendertargetDM==resp.data.ims[t].user){
-                                        finalUser = resp.data.ims[t].id;
-                                        finalUserId = resp.data.ims[t].user;
-                                        axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
-                                            token: config('POST_BOT_TOKEN'),
-                                            channel: finalUser,
-                                            user:finalUserId,
-                                            as_user:true,                                        
-                                            attachments: JSON.stringify([
-                                              {
-                                                title: "Forwarded",
-                                                color: ORANGE,
-                                                text: "Task ID: " + taskNumber + "\n Title: " + result.rows[0].title + "\n Recipient: " + result.rows[0].receiver_id +  " Forwarder: " + forwarder + " Owner: " + result.rows[0].sender_id,
-                                                callback_id: "forwardDialogMsg",
-                                              },
-                                            ]),
-                                        })).then((result) => {
-                                            //console.log('sendConfirmation: ', result.data); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
-                                        }).catch((err) => {
-                                            //console.log('sendConfirmation error: ', err); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
-                                        });
-                                    }
-                                }
-                            }).catch(function (err){
-                                //console.log(err); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
+                                channel: finalUser,
+                                user:finalUserId,
+                                as_user:true,
+                                attachments: JSON.stringify([
+                                  {
+                                    title: "Forwarded",
+                                    color: ORANGE,
+                                    text: "Task ID: " + taskNumber + "\n Title: " + result.rows[0].title + "\n Recipient: " + result.rows[0].receiver_id +  " Forwarder: " + forwarder + " Owner: " + result.rows[0].sender_id,
+                                    callback_id: "askDialogHandler",
+                                    actions: buttons
+                                  },
+                                ]),
+                            })).then((result) => {
+                                //console.log('sendConfirmation: ', result.data); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
+                            }).catch((err) => {
+                                //console.log('sendConfirmation error: ', err); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
                             });
-                            sendMessage("Forwarded", "", ORANGE);
-                    });
-                }
+                        }
+                    }
+                }).catch(function (err){
+                    //console.log(err); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
+                });
+
+                //DM to the owner of the task
+                axios.post('https://slack.com/api/im.list', qs.stringify({
+                    token: config('POST_BOT_TOKEN'),
+                })).then(function (resp){
+                    //console.log(resp.data); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
+                    for(var t = 0; t < resp.data.ims.length; t++){
+                        //console.log(resp.data.ims[t].id); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
+                        if(sendertargetDM==resp.data.ims[t].user){
+                            finalUser = resp.data.ims[t].id;
+                            finalUserId = resp.data.ims[t].user;
+                            axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+                                token: config('POST_BOT_TOKEN'),
+                                channel: finalUser,
+                                user:finalUserId,
+                                as_user:true,                                        
+                                attachments: JSON.stringify([
+                                  {
+                                    title: "Forwarded",
+                                    color: ORANGE,
+                                    text: "Task ID: " + taskNumber + "\n Title: " + result.rows[0].title + "\n Recipient: " + result.rows[0].receiver_id +  " Forwarder: " + forwarder + " Owner: " + result.rows[0].sender_id,
+                                    callback_id: "forwardDialogMsg",
+                                  },
+                                ]),
+                            })).then((result) => {
+                                //console.log('sendConfirmation: ', result.data); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
+                            }).catch((err) => {
+                                //console.log('sendConfirmation error: ', err); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
+                            });
+                        }
+                    }
+                }).catch(function (err){
+                    //console.log(err); //#DEBUG CODE: UNCOMMENT FOR DEBUGGING PURPOSES ONLY
+                });
+                sendMessage("Forwarded", "", ORANGE);
         });
     }).catch((err) => {
         sendMessage("*** ERROR ***", "" + err, RED);
