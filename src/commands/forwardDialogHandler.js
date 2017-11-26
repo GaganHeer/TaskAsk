8a5b2860db6421064e179a30bb866959c43589b7
Gagandeep Heer <gagandeep_heer29@hotmail.com>
2017-11-25 21:38:47 -0800

    forward dialog refactor

--- src/commands/forwardDialogHandler.js
+++ src/commands/forwardDialogHandler.js
@@ -71,6 +71,7 @@ const handler = (payload, res) => {
                 });
 
                 //DM to the owner of the task
+            console.log("IM HERE " + sendertargetDM);
                 axios.post('https://slack.com/api/im.list', qs.stringify({
                     token: config('POST_BOT_TOKEN'),
                 })).then(function (resp){
